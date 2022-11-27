269908f319331cd2d45a0622bd846012760d4577
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-27 18:37:13 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -4,4 +4,4 @@
 docker rm ep
 docker rmi ep
 docker build --tag ep .
-docker run -e TYPE="master" e MADDR=localhost -e MPORT=7087 -e NREPLICAS=3 --name ep ep
+docker run -e TYPE="master" e MADDR=localhost -e MPORT=7087 -e NREPLICAS=3 --name ep ep:latest
