cea58f30eb1295b9fd2d28f046df5e6764e94f05
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-28 04:02:59 -0500

    save master

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #bash bin/initMaster.sh
 
-MIP=172.75.0.1
+MIP="172.75.0.2"
 MASTERNAME="master"
 docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
 docker network rm Paxos_Network
 docker build --tag ${MASTERNAME} .
 docker network create --subnet=172.75.0.0/16 Paxos_Network
-docker run --net Paxos_Network --ip=172.75.0.2 -p 7087:7087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=7087 -e NREPLICAS=3 --name ${MASTERNAME} ${MASTERNAME}
+docker run --net Paxos_Network --ip=${MIP}} -p 7087:7087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=7087 -e NREPLICAS=3 --name ${MASTERNAME} ${MASTERNAME}
