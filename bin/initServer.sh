96a944ad02b7b9facd1380729a2a314f8a753f93
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-28 04:38:16 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -3,9 +3,9 @@
 MIP="172.75.0.2"
 MASTERNAME="master"
 MASTERPORT=7087
-SERVERNAME="server4"  #modify serer name
-SERVERIP="172.75.0.7" #ip start with IP address 172.75.0.4
-SERVERPORT=7073 #port start with 7070
+SERVERNAME="server5"  #modify serer name
+SERVERIP="172.75.0.8" #ip start with IP address 172.75.0.4
+SERVERPORT=7074 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
