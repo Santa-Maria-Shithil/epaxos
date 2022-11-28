4d5103469205a6fbc4ca27a5398445dc40f36022
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-27 19:13:59 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -4,5 +4,5 @@
 docker rm ep
 docker rmi ep
 docker build --tag ep .
--docker network create Paxos_Network
+docker network create Paxos_Network
 docker run -p 7087:7087 -e TYPE="master" -e MADDR=localhost -e MPORT=7087 -e NREPLICAS=3 --name ep --net Paxos_Network ep
