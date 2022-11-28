39fc3afcc8c15fa504e5b72d1ddf8fa967ba023a
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-28 03:51:15 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #bash bin/initServer.sh
-SERVERNAME="server1"  #modify serer name
+SERVERNAME="server2"  #modify serer name
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --ip=172.75.0.4 -p 7070:7070 -d -e SPORT="7070" -e TYPE="server" --name ${SERVERNAME} --net Paxos_Network ${SERVERNAME}
+docker run --ip=172.75.0.5 -p 7071:7071 -d -e SPORT="7070" -e TYPE="server" --name ${SERVERNAME} --net Paxos_Network ${SERVERNAME}
 #modify -p  and -SPORT option, --ip start with IP address 172.75.0.4
