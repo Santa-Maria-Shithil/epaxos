2f5b18ddea59e93c1b9ff6a19e420602dd508101
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-11-27 19:28:21 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #bash bin/initServer.sh
-SERVERNAME="server1"  #modify serer name
+SERVERNAME="server2"  #modify serer name
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run -p 7070:7070 -d -e TYPE="server" --name ${SERVERNAME} --net Paxos_Network ${SERVERNAME}
+docker run -p 7071:7071 -d -e TYPE="server" --name ${SERVERNAME} --net Paxos_Network ${SERVERNAME}
 #modify port number
