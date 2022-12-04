cbd0211466c41c4a7baebac6294e1666cacdae5e
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-03 21:56:34 -0500

    update

--- src/client/client.go
+++ src/client/client.go
@@ -17,7 +17,7 @@ import (
 
 var masterAddr *string = flag.String("maddr", "", "Master address. Defaults to localhost")
 var masterPort *int = flag.Int("mport", 7087, "Master port.  Defaults to 7077.")
-var reqsNb *int = flag.Int("q", 5000, "Total number of requests. Defaults to 5000.")
+var reqsNb *int = flag.Int("q", 100000, "Total number of requests. Defaults to 5000.")
 var writes *int = flag.Int("w", 100, "Percentage of updates (writes). Defaults to 100%.")
 var noLeader *bool = flag.Bool("e", false, "Egalitarian (no leader). Defaults to false.")
 var fast *bool = flag.Bool("f", false, "Fast Paxos: send message directly to all replicas. Defaults to false.")
