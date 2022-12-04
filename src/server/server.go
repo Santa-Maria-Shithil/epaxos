4f34781221e9969f005319a1cf4fc81e8a3a9dbc
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-03 22:08:16 -0500

    update

--- src/server/server.go
+++ src/server/server.go
@@ -57,19 +57,19 @@ func main() {
 	replicaId, nodeList := registerWithMaster(fmt.Sprintf("%s:%d", *masterAddr, *masterPort))
 
 	if *doEpaxos {
-		log.Println("Starting Egalitarian Paxos replica...")
+		log.Printf("Starting Egalitarian Paxos replica...")
 		rep := epaxos.NewReplica(replicaId, nodeList, *thrifty, *exec, *dreply, *beacon, *durable)
 		rpc.Register(rep)
 	} else if *doMencius {
-		log.Println("Starting Mencius replica...")
+		log.Printf("Starting Mencius replica...")
 		rep := mencius.NewReplica(replicaId, nodeList, *thrifty, *exec, *dreply, *durable)
 		rpc.Register(rep)
 	} else if *doGpaxos {
-		log.Println("Starting Generalized Paxos replica...")
+		log.Printf("Starting Generalized Paxos replica...")
 		rep := gpaxos.NewReplica(replicaId, nodeList, *thrifty, *exec, *dreply)
 		rpc.Register(rep)
 	} else {
-		log.Println("Starting classic Paxos replica...")
+		log.Printf("Starting classic Paxos replica...")
 		rep := paxos.NewReplica(replicaId, nodeList, *thrifty, *exec, *dreply, *durable)
 		rpc.Register(rep)
 	}
