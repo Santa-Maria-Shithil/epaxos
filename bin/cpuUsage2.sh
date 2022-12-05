cdec746c6c8a87823c02e9694c097aeebceb24e3
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 02:05:56 -0500

    update

--- bin/cpuUsage2.sh
+++ bin/cpuUsage2.sh
@@ -4,7 +4,7 @@ while true; do
         #mpstat -P 1,2,3,4,5,6,7,8,9,10,11,12,13,14  2>&1 | tee -a cpuUsage.txt
         #mkdir -p cpuInfo/
         sudo docker stats 2>&1 | tee -a cpuUsage.txt 
-        #sleep 1
+        sleep 1
        # echo q | htop -d 10| aha --black --line-fix > cpuInfo/htop${i}.html
        # ((i=i+1))
     done
