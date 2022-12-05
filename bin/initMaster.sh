8447b8d3c31af33c10746fbc9898ea9499d65e6a
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 03:46:49 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #bash bin/initMaster.sh
 
-MIP="172.76.0.2"
-MASTERNAME="master"
+MIP="172.77.0.2"
+MASTERNAME="master2"
 docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
-docker network rm Paxos_Network1
+docker network rm Paxos_Network2
 docker build --tag ${MASTERNAME} .
-docker network create --subnet=172.76.0.0/16 Paxos_Network1
-docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network1 --ip=${MIP} -p 7087:7087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=7087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
+docker network create --subnet=172.77.0.0/16 Paxos_Network2
+docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network2 --ip=${MIP} -p 7087:7087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=7087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #bash bin/initServer.sh
-MIP="172.76.0.2"
+MIP="172.77.0.2"
 MASTERNAME="master"
 MASTERPORT=7087
-SERVERNAME="server1"  #modify serer name
-SERVERIP="172.76.0.4" #ip start with IP address 172.75.0.4
+SERVERNAME="server2.1"  #modify serer name
+SERVERIP="172.77.0.4" #ip start with IP address 172.75.0.4
 SERVERPORT=7070 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --cpuset-cpus="35-42" --memory="32g" --net Paxos_Network1 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="35-42" --memory="32g" --net Paxos_Network2 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
