dd4973680e2c0f3fb272602a298ea6b10d9fb86e
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 00:58:55 -0500

    update

--- bin/cpuUsage2.sh
+++ bin/cpuUsage2.sh
@@ -1,5 +1,7 @@
 
+i=0
 while true; do
-        mpstat -P 1,2,3,4,5,6,7,8,9,10,11,12,13,14  2>&1 | tee -a cpuUsage.txt
-        sleep 10;
+        #mpstat -P 1,2,3,4,5,6,7,8,9,10,11,12,13,14  2>&1 | tee -a cpuUsage.txt
+        echo q | htop --time=10| aha --black --line-fix > cpuInfor/htop${i}.html
+        i++
     done
