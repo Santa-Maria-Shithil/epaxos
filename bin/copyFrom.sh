93a85f4e402140386f270317fdb661810f1ea712
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 05:51:18 -0500

    udpate

--- bin/copyFrom.sh
+++ bin/copyFrom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..90}
 do
-   sudo docker cp client3:/epaxos/logs/c_${i}.txt /home/users/sshithil/epaxos/logs/c_${i}.txt
+   sudo docker cp client4:/epaxos/logs/c_${i}.txt /home/users/sshithil/epaxos/logs/c_${i}.txt
 done
