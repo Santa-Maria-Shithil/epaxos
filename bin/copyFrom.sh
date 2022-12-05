cae50f64883903c810d7736217c856b2024558e7
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 05:47:39 -0500

    update

--- bin/copyFrom.sh
+++ bin/copyFrom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {1..90}
 do
-   sudo docker cp client5:/epaxos/logs/c_${i}.txt /home/users/sshithil/epaxos/logs/c_${i}.txt
+   sudo docker cp client1:/epaxos/logs/c_${i}.txt /home/users/sshithil/epaxos/logs/c_${i}.txt
 done
