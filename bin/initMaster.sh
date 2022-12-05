0f42ed5ea79abc3d8ef3fda2074a85df67b4e5f8
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 16:54:51 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #bash bin/initMaster.sh
 
-MIP="172.80.0.2"
-MASTERNAME="master5"
+MIP="172.76.0.2"
+MASTERNAME="master1"
 docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
-docker network rm Paxos_Network5
+docker network rm Paxos_Network1
 docker build --tag ${MASTERNAME} .
-docker network create --subnet=172.80.0.0/16 Paxos_Network5
+docker network create --subnet=172.76.0.0/16 Paxos_Network5
 docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network5 --ip=${MIP} -p 5087:5087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=5087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #bash bin/initServer.sh
-MIP="172.80.0.2"
-MASTERNAME="master5"
+MIP="172.76.0.2"
+MASTERNAME="master1"
 MASTERPORT=5087
-SERVERNAME="server5.7"  #modify serer name
-SERVERIP="172.80.0.8" #ip start with IP address 172.75.0.4
-SERVERPORT=5076 #port start with 7070
+SERVERNAME="server1.1"  #modify serer name
+SERVERIP="172.76.0.4" #ip start with IP address 172.75.0.4
+SERVERPORT=5070 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --cpuset-cpus="27-34" --memory="32g" --net Paxos_Network5 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="3-10" --memory="32g" --net Paxos_Network5 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
