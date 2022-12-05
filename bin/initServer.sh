023a44f29f9fed7683c66df68cacd3b35c0d4dd0
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 03:56:34 -0500

    update

--- bin/initServer.sh
+++ bin/initServer.sh
@@ -3,10 +3,10 @@
 MIP="172.77.0.2"
 MASTERNAME="master2"
 MASTERPORT=8087
-SERVERNAME="server2.2"  #modify serer name
-SERVERIP="172.77.0.5" #ip start with IP address 172.75.0.4
-SERVERPORT=8071 #port start with 7070
+SERVERNAME="server2.3"  #modify serer name
+SERVERIP="172.77.0.6" #ip start with IP address 172.75.0.4
+SERVERPORT=8072 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --cpuset-cpus="3-10" --memory="32g" --net Paxos_Network2 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="19-26" --memory="32g" --net Paxos_Network2 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
