5335948a3f08583013b814bd9b3be6e7c478034c
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 04:34:53 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #bash bin/initMaster.sh
 
-MIP="172.79.0.2"
-MASTERNAME="master4"
+MIP="172.80.0.2"
+MASTERNAME="master5"
 docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
-docker network rm Paxos_Network4
+docker network rm Paxos_Network5
 docker build --tag ${MASTERNAME} .
-docker network create --subnet=172.79.0.0/16 Paxos_Network4
-docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network4 --ip=${MIP} -p 6087:6087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=6087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
+docker network create --subnet=172.80.0.0/16 Paxos_Network5
+docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network5 --ip=${MIP} -p 5087:5087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=5087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #bash bin/initServer.sh
-MIP="172.79.0.2"
-MASTERNAME="master4"
-MASTERPORT=6087
-SERVERNAME="server4.5"  #modify serer name
-SERVERIP="172.79.0.8" #ip start with IP address 172.75.0.4
-SERVERPORT=6075 #port start with 7070
+MIP="172.80.0.2"
+MASTERNAME="master5"
+MASTERPORT=5087
+SERVERNAME="server5.1"  #modify serer name
+SERVERIP="172.80.0.4" #ip start with IP address 172.75.0.4
+SERVERPORT=5070 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --cpuset-cpus="35-42" --memory="32g" --net Paxos_Network4 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="3-10" --memory="32g" --net Paxos_Network5 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
