bf96a8a2fac1e7c0687149dfc3463034ec4ce645
Santa-Maria-Shithil <sshithil@cs.stonybrook.edu>
2022-12-05 04:05:48 -0500

    update

--- bin/initMaster.sh
+++ bin/initMaster.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #bash bin/initMaster.sh
 
-MIP="172.77.0.2"
-MASTERNAME="master2"
+MIP="172.78.0.2"
+MASTERNAME="master3"
 docker rm ${MASTERNAME}
 docker rmi ${MASTERNAME}
-docker network rm Paxos_Network2
+docker network rm Paxos_Network3
 docker build --tag ${MASTERNAME} .
-docker network create --subnet=172.77.0.0/16 Paxos_Network2
-docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network2 --ip=${MIP} -p 8087:8087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=8087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
+docker network create --subnet=172.78.0.0/16 Paxos_Network3
+docker run --cpuset-cpus="1,2" --memory="8g" --net Paxos_Network3 --ip=${MIP} -p 9087:9087 -e TYPE="master" -e MADDR=${MIP} -e MPORT=9087 -e NREPLICAS=5 --name ${MASTERNAME} ${MASTERNAME}
--- bin/initServer.sh
+++ bin/initServer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #bash bin/initServer.sh
-MIP="172.77.0.2"
-MASTERNAME="master2"
-MASTERPORT=8087
-SERVERNAME="server2.5"  #modify serer name
-SERVERIP="172.77.0.8" #ip start with IP address 172.75.0.4
-SERVERPORT=8074 #port start with 7070
+MIP="172.78.0.2"
+MASTERNAME="master3"
+MASTERPORT=9087
+SERVERNAME="server3.1"  #modify serer name
+SERVERIP="172.78.0.4" #ip start with IP address 172.75.0.4
+SERVERPORT=9074 #port start with 7070
 docker rm ${SERVERNAME}
 docker rmi ${SERVERNAME}
 docker build --tag ${SERVERNAME} .
-docker run --cpuset-cpus="35-42" --memory="32g" --net Paxos_Network2 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
+docker run --cpuset-cpus="35-42" --memory="32g" --net Paxos_Network3 --ip=${SERVERIP} -p ${SERVERPORT}:${SERVERPORT} -e MADDR=${MIP} -e MPORT=${MASTERPORT} -e SADDR=${SERVERIP} -e SPORT=${SERVERPORT} -e TYPE="server" --name ${SERVERNAME} ${SERVERNAME}
